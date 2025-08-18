7c783a1226bcd8ad421b2957ef5c9baee274e737
ramis-khasianov <ramis.khasianov@gmail.com>
2025-08-18 12:48:13 +0500

    layout background

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="relative bg-light-900 dark:bg-dark-100">
+    <main className="relative bg-light-850 dark:bg-dark-100">
       <Navbar />
       <div className="flex min-h-screen flex-1 flex-col background-light850_dark100 px-6 pb-6 pt-36">
         {children}
