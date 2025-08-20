aa919ff7c864f985a29de4d157092dd2d20b0b5b
ramis-khasianov <ramis.khasianov@gmail.com>
2025-08-20 19:24:15 +0500

    user related navbar

---
+++ components/navbar/UserProfile.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { User } from "lucide-react";
+import { Session } from "next-auth";
+import { signOut } from "next-auth/react";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import ROUTES from "@/constants/routes";
+import { useRouter } from "next/navigation";
+
+interface UserProfileProps {
+  session: Session;
+}
+
+const UserProfile = ({ session }: UserProfileProps) => {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await signOut({ redirect: false });
+    toast.success("Signed out successfully!");
+    router.push(ROUTES.HOME);
+    router.refresh();
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="no-focus">
+        <Button variant="ghost" size="icon">
+          <User className="size-[1.2rem]" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">
+              {session.user?.name || session.user?.email}
+            </p>
+            {session.user?.email && (
+              <p className="text-xs leading-none text-muted-foreground">
+                {session.user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => router.push("/profile")}>
+          Profile
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={handleSignOut} variant="destructive">
+          Sign out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserProfile;
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,18 +1,23 @@
 import Link from "next/link";
-
+import { auth } from "@/auth";
 import ThemeToggle from "./ThemeToggle";
+import UserProfile from "./UserProfile";
+import AuthButtons from "./AuthButtons";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
-    <nav className="flex justify-between items-center bg-light-900 dark:bg-dark-200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className="flex justify-between items-center bg-light-900 dark:bg-dark-200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12 px-6">
       <Link href="/" className="flex items-center gap-1">
-        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900">
           Raports <span className="text-primary-gradient">Blog</span>
         </p>
       </Link>
 
-      <div className="flex-between gap-5">
+      <div className="flex flex-between gap-2">
         <ThemeToggle />
+        {session?.user ? <UserProfile session={session} /> : <AuthButtons />}
       </div>
     </nav>
   );
