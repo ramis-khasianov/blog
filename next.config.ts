23ff7ed08f5e73aa8d2d9bf5da2a042e45f65b2c
ramis-khasianov <ramis.khasianov@gmail.com>
2025-09-18 12:34:50 +0500

    blob storage

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
